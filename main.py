451e56df2cd4e1d326cc20a248a40af7f4fae9ea
Trishanu Roy <gtrishanu@gmail.com>
2023-11-29 23:16:41 +0530

    adding all the files

---
+++ main.py
@@ -0,0 +1,48 @@
+import openai
+import json
+import os
+import streamlit as st
+from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, ServiceContext
+
+os.environ['OPENAI_API_KEY'] = "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"
+
+documents = SimpleDirectoryReader('/Users/trishanu/Downloads/Phycology_LLM').load_data()
+index = GPTVectorStoreIndex(documents)
+
+class Chatbot:
+    def __init__(self, api_key, index):
+        self.index = index
+        openai.api_key = api_key
+        self.chat_history = []
+
+    def generate_response(self, user_input):
+        prompt = "\n".join([f"{message['role']}: {message['content']}" for message in self.chat_history[-5:]])
+        prompt += f"\nUser: {user_input}"
+        query_engine = index.as_query_engine()
+        response=query_engine.query(user_input)
+
+        message = {"role": "assistant", "content": response.response}
+        self.chat_history.append({"role": "user", "content": user_input})
+        self.chat_history.append(message)
+        return message
+
+    def load_chat_history(self, filename):
+        try:
+            with open(filename, 'r') as f:
+                self.chat_history = json.load(f)
+        except FileNotFoundError:
+            pass
+
+    def save_chat_history(self, filename):
+        with open(filename, 'w') as f:
+            json.dump(self.chat_history, f)
+
+bot = Chatbot("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", index=index)
+bot.load_chat_history("chat_history.json")
+
+st.title('Phycology Chat Bot')
+
+user_input = st.text_input("You: ", "")
+if st.button("Send"):
+    response = bot.generate_response(user_input)
+    st.write(f"Bot: {response['content']}")
